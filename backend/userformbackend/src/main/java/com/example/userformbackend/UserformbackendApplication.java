40cbc2c4c580d4dfc441dd4033832fcd643a642a
vivek <vivekneymar2002@gmail.com>
2024-11-12 03:38:39 +0530

    initial comit backend

---
+++ backend/userformbackend/src/main/java/com/example/userformbackend/UserformbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.userformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserformbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserformbackendApplication.class, args);
+	}
+
+}
---
+++ backend/userformbackend/src/test/java/com/example/userformbackend/UserformbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserformbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
